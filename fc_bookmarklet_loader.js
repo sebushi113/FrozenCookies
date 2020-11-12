8e09ba57429f8d5288e800e6cd08d73ed38ad10e
sebushi113 <41740143+sebushi113@users.noreply.github.com>
2020-11-12 17:25:21 -0500

    link change

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -1,5 +1,5 @@
 javascript:(
     function () {
-        Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
+        Game.LoadMod('https://sebushi113.github.io/FrozenCookies/frozen_cookies.js');
     }()
 );
