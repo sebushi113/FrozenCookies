7aa7c866192d3bab5710d8305e2fef5284f87822
sebushi113 <41740143+sebushi113@users.noreply.github.com>
2020-11-12 17:22:46 -0500

    change location

--- fc_userscript_loader.user.js
+++ fc_userscript_loader.user.js
@@ -6,17 +6,17 @@
 // @homepage       https://github.com/Mtarnuhal/FrozenCookies
 // @include        http://orteil.dashnet.org/cookieclicker/
 // @include        https://orteil.dashnet.org/cookieclicker/
-// @updateURL      https://mtarnuhal.github.io/FrozenCookies/fc_userscript_loader.user.js
-// @downloadURL    https://mtarnuhal.github.io/FrozenCookies/fc_userscript_loader.user.js
+// @updateURL      https://sebushi113.github.io/FrozenCookies/fc_userscript_loader.user.js
+// @downloadURL    https://sebushi113.github.io/FrozenCookies/fc_userscript_loader.user.js
 // @run-at         document-start
 // ==/UserScript==
 
-// Dev:       https://github.com/Mtarnuhal/FrozenCookies/development/
-// Master:    https://github.com/Mtarnuhal/FrozenCookies/master/
+// Dev:       https://github.com/sebushi113/FrozenCookies/development/
+// Master:    https://github.com/sebushi113/FrozenCookies/master/
 // Github.io: http://lordshinjo.github.io/FrozenCookies/
 
 function LoadFrozenCookies() {
-    Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
+    Game.LoadMod('https://sebushi113.github.io/FrozenCookies/frozen_cookies.js');
 }
 
 window.addEventListener("load", LoadFrozenCookies, false);
